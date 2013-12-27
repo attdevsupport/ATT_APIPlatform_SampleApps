71cfd89124e18529682a7746bb989c1828218f40
Pablo Padilla <pgpb.padilla@gmail.com>
2013-12-27 14:36:33 -0800

    Updating Ads Sample app with AdsAPI v2.0.3

--- Android/ADS/src/com/att/ads/sample/AdsViewActivity.java
+++ Android/ADS/src/com/att/ads/sample/AdsViewActivity.java
@@ -98,8 +98,8 @@ public class AdsViewActivity extends Activity implements ATTAdViewListener {
 		} catch (Exception e) {
 			Log.e(TAG, "Eror :" + e.fillInStackTrace());
 		}*/
-		appKey = UPDATE_APP_KEY_HERE;
-		secret = UPDATE_SECRET_KEY_HERE;
+		appKey = "UPDATE_APP_KEY_HERE";
+		secret = "UPDATE_SECRET_KEY_HERE";
 		
 		// Category always in lower case
 		if(null == appKey || null == secret){
